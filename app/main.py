af460d0a7d52cd8529b2b73abbf0fa26e66024fd
kovcharovturgunbek-dev <kovcharovturgunbek@gmail.com>
2026-08-22 01:38:11 +0500

    main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,36 @@
 import asyncio
 import os
 import threading
+
+import uvicorn
 from aiogram import Bot, Dispatcher
 from fastapi import FastAPI
-import uvicorn
 
 from app.cache.redis import close_redis
-from app.core.logging import setup_logging
 from app.core.config import settings
-from app.handlers import router as main_router  # Routerimizni ulaymiz
+from app.core.logging import setup_logging
+from app.db.session import close_database
+from app.handlers import router as main_router
+
 
 app_web = FastAPI()
 
+
 @app_web.get("/")
-def root():
+async def root():
     return {"status": "Bot ishlayapti!"}
 
-def start_fastapi():
+
+def start_fastapi() -> None:
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app_web, host="0.0.0.0", port=port, log_level="warning")
+
+    uvicorn.run(
+        app_web,
+        host="0.0.0.0",
+        port=port,
+        log_level="warning",
+    )
+
 
 async def asosiy() -> None:
     setup_logging()
@@ -26,7 +38,6 @@ async def asosiy() -> None:
     bot = Bot(token=settings.bot_token)
     dp = Dispatcher()
 
-    # Routerlarni Dispatcher'ga ulaymiz
     dp.include_router(main_router)
 
     try:
@@ -34,9 +45,14 @@ async def asosiy() -> None:
     finally:
         await bot.session.close()
         await close_redis()
+        await close_database()
 
-if __name__ == "__main__":
-    server_thread = threading.Thread(target=start_fastapi, daemon=True)
+
+if name == "main":
+    server_thread = threading.Thread(
+        target=start_fastapi,
+        daemon=True,
+    )
     server_thread.start()
 
     try:
