9dc7c10abbce503e199e02da164cce30c1554a12
kovcharovturgunbek-dev <kovcharovturgunbek@gmail.com>
2026-08-21 23:35:30 +0500

    main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 
-from ilova.cache.redis import close_redis
+from app.cache.redis import close_redis
 from app.core.logging import setup_logging
-from ilova.yadro.konfiguratsiya import sozlamalar
+from app.yadro.konfiguratsiya import sozlamalar
 
 # Routerlaringizni shu yerga import qilasiz va ulaysiz:
-# Masalan: from ilova.routerlar import main_router
+# Masalan: from app.routerlar import main_router
 
 
 async def asosiy() -> None:
