c63909fda6615297dbc696f3a3c18444b8603be3
kovcharovturgunbek-dev <kovcharovturgunbek@gmail.com>
2026-08-21 23:52:33 +0500

    main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 
 from app.cache.redis import close_redis
 from app.core.logging import setup_logging
-from app.core.konfiguratsiya import sozlamalar
+from app.core.config import settings
 
 # Routerlaringizni shu yerga import qilasiz va ulaysiz:
 # Masalan: from app.routerlar import main_router
